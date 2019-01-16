6060f4e6a4ee08894c7d1b9afc00fa4a1dc95bf6
Anzor007 <AnzorZhukov@gmail.com>
2019-01-17 00:50:56 +0300

    fin task oops)))

--- task1.js
+++ task1.js
@@ -1,42 +1,71 @@
 //var pick01 = require('./test_01.js');
- 
-var arrCourse = {};       //учебный курс
-var arrTheme = {};   //тема учебного курса
-var arrIssues = {};   //вопросы учебного курса
-//var arrAnswers = {};   //ответы учебного курса
 
-class Issues { //класс вопросов
 
-    constructor (iss01) {
+class Question { //класс вопросов
 
-        this.iss01 = iss01;
+    constructor (question,answersOk) {
+
+        this.question = question;
+        this.answersNo = [];
+        this.answersOk = [answersOk];
+    }
+    addAnswer(answer, isTrue) {
+
+        isTrue ? this.answersOk.push(answer) : this.answersNo.push(answer) ;
 
     }
-
 };
 
+var q = new Question('chto za fignya 1','nifiga');
+q.addAnswer("chiconya", 0);
+q.addAnswer("chico", 1);
+q.addAnswer("chnya", 1);
+q.addAnswer("chiconya", 0);
+q.addAnswer("chiconya", 0);
+q.addAnswer("chiconya", 0);
+
+console.log(q);
+console.log("-------\n",Object.keys(q));
 
 class Theme { // класс тем
 
-    constructor (the01) {
+    constructor(theme, question) {
 
-        this.the01 = the01;
+        this.theme = theme;
+        this.question = [question];
+
+    }
+
+    get questions() {       //получаем знач
+        return this.question;
+    }
+    set questions(question) {   //устанавливаем значение
+        this.question.push(question);
+    }
+    //set question
+};
+var t = new Theme('tema pampers', q);
+t.questions = q; //set
+console.log(" --1-t-\n", t.questions);  //get
+console.log(" --2-t-\n", t); //instance
+
+class Course extends Theme { //класс курса
+
+    constructor (course, theme) {
+
+        super(course,theme);
 
     }
 
 };
 
+var c = new Course('prevedD medvedD', t);
 
-class Course { //класс курса
-
-    constructor (cou01) {
-
-        this.cou01 = cou01;
-
-    }
-
-};
-
+c.questions = t; //set
+console.log(" --1-c-\n", c.questions);  //get
+console.log(" --2-c-\n" + c); //instance
+console.debug(JSON.stringify(c, null, 1));
+/*
 class Answer { // класс ответов
     constructor (name) {
 
@@ -47,19 +76,19 @@ class Answer { // класс ответов
             this.arrAnswers.push(name);
 
         }
-        
+
     }
 
 };
 
 // создаем экземпляры класса
-var ans1 = new Answer();        
+var ans1 = new Answer();
 ans1.add('otvet_0');
 ans1.add('otvet_1');
 ans1.add('otvet_2');
 console.log('ответы  =', ans1.arrAnswers);
 
-
+*/
 
 
 //module.exports = catFactoryL;
